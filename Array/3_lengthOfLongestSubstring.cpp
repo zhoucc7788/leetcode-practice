7892858892d2904b772afc96bfafd07f40300fc4
zhoucc7788 <72068251+zhoucc7788@users.noreply.github.com>
2023-06-11 19:41:40 +0800

    大佬动态算法

--- Array/3_lengthOfLongestSubstring.cpp
+++ Array/3_lengthOfLongestSubstring.cpp
@@ -3,56 +3,75 @@
 #include <string>
 #include <memory>
 #include <vector>
-#include <sstream>
 #include <algorithm>
+#include <unordered_map>
 
-//根据其他答案的改进思路：实时存储每个出现过的字母第一次出现的位置，而不是发现有重复的再回去找。
-//用哈希表来存储已经出现过的字符，然后字符作为键，字符第一次出现的位置作为值可能会更好
-//最好的就是不需要复制出一个子串，然后不停插入又清空，而是用两个坐标标识起始位和终止位就行
+//大佬算法，规划是动态的而不是找到了重复的才规划
 class Solution {
-public:  
-
-    std::vector<char> strset = {};
+public:
 
     int lengthOfLongestSubstring(std::string s) {
+        int head = 0, tail = 0, result = 0;
+        std::unordered_map<char, int> index_map = {};
 
-        std::string::iterator head = s.begin();
-        std::string::iterator ptr = head;
-        int maxLen = 0;
-        
-        while (ptr != s.end()) {
-            if (find(*ptr) == strset.end()){
-                strset.emplace_back(*ptr);   //还是搞不太清楚push_back和emplace_back的区别
-                ptr++;
+        while (tail < s.size()) {
+            if (index_map.find(s[tail]) != index_map.end()) {
+                head = std::max(index_map[s[tail]], head);//如果目前指向的这个字母最新一次出现的地方大于head，说明实质上已经重复了，把head的位置更新，本次循环就会更新tail-head+1的结果
             }
-            else {
-                if(strset.size() > maxLen) maxLen = strset.size();
-                ptr = head += std::distance(strset.begin(), find(*ptr))+1;    //迭代器往后移动到第一个重复的字母的下一位，\
-                distance返回的相当于是两个元素的序号的差，比如第一个元素和第三个元素里就会差2，所以如果只加2会刚好到出现重复的那个字母上，还要向后一位才对
-                strset.clear();
-            }
+
+            result = std::max(result, tail - head + 1);//每次循环都会判断，+1是因为假如这次还没有找到重复的，那现在tail停在的字符上也要被算到无重复子串长度里（也可以理解为因为减法不会把开头算进去所以需要+1）
+            index_map[s[tail]] = tail + 1;    //!!!重要的是每个字母对应的index在是它们在字符串里的顺序+1，好处是以这个index去索引字符串刚好可以索引到下一位 \
+                        而head是正常的索引
+            tail++;
         }
 
-        if (strset.size() > maxLen) maxLen = strset.size();
-        return maxLen;
+        return result;
     }
-
-    std::vector<char>::iterator find(char element) 
-    {
-        std::vector<char>::iterator  it = strset.begin();
-        
-        while (it != strset.end()) {
-            if (*it == element) {
-                return it;
-            }
-            else {
-                it++;
-            }
-        }
-        return strset.end();
-    }
-
 };
+//class Solution {
+//public:  
+//
+//    std::vector<char> strset = {};
+//
+//    int lengthOfLongestSubstring(std::string s) {
+//
+//        std::string::iterator head = s.begin();
+//        std::string::iterator ptr = head;
+//        int maxLen = 0;
+//        
+//        while (ptr != s.end()) {
+//            if (find(*ptr) == strset.end()){
+//                strset.emplace_back(*ptr);   //还是搞不太清楚push_back和emplace_back的区别
+//                ptr++;
+//            }
+//            else {
+//                if(strset.size() > maxLen) maxLen = strset.size();
+//                ptr = head += std::distance(strset.begin(), find(*ptr))+1;    //迭代器往后移动到第一个重复的字母的下一位，\
+//                distance返回的相当于是两个元素的序号的差，比如第一个元素和第三个元素里就会差2，所以如果只加2会刚好到出现重复的那个字母上，还要向后一位才对
+//                strset.clear();
+//            }
+//        }
+//
+//        if (strset.size() > maxLen) maxLen = strset.size();
+//        return maxLen;
+//    }
+//
+//    std::vector<char>::iterator find(char element) 
+//    {
+//        std::vector<char>::iterator  it = strset.begin();
+//        
+//        while (it != strset.end()) {
+//            if (*it == element) {
+//                return it;
+//            }
+//            else {
+//                it++;
+//            }
+//        }
+//        return strset.end();
+//    }
+//
+//};
 //这个会稍微慢一点点
 //class Solution {
 //public:
