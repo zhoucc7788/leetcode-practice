b61229f4e0442f73c22e9a2f9a4bba241129ec48
zcc <cufezhoucc@163.com>
2023-07-05 18:36:16 +0800

    pause

--- 2_addTwoNumbers/src/addTwoNumbers.cpp
+++ 2_addTwoNumbers/src/addTwoNumbers.cpp
@@ -4,5 +4,15 @@
 
 ListNode* Solution::addTwoNumbers(ListNode* l1, ListNode* l2){
     int march = 0;
-    while ()
+    std::shared_ptr<ListNode> result = std::make_shared<ListNode>();
+    std::shared_ptr<ListNode> cur = result;
+    
+   while (l1 != nullptr || l2 != nullptr){
+
+        result->next =std::make_shared
+        
+        
+        l1 = l1->next == nullptr ? nullptr : l1->next;
+        l2 = l2->next == nullptr ? nullptr : l2->next;
+    }
 }
