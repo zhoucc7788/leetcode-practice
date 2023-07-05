a1aba798c6134f449f6ac44695f172698ee33e1d
zcc <cufezhoucc@163.com>
2023-07-05 20:52:01 +0800

    pause

--- 2_addTwoNumbers/src/addTwoNumbers.cpp
+++ 2_addTwoNumbers/src/addTwoNumbers.cpp
@@ -3,16 +3,21 @@
 #include "addTwoNumbers.h"
 
 ListNode* Solution::addTwoNumbers(ListNode* l1, ListNode* l2){
+    int digit = 0;
     int march = 0;
-    std::shared_ptr<ListNode> result = std::make_shared<ListNode>();
-    std::shared_ptr<ListNode> cur = result;
+    ListNode* result = new ListNode();
+    ListNode* cur = result;
     
    while (l1 != nullptr || l2 != nullptr){
 
-        result->next =std::make_shared
-        
-        
+        digit = ((l1 != nullptr ? l1->val : 0) + (l2 != nullptr ? l2->val : 0) + march) % 10;
+        march = ((l1 != nullptr ? l1->val : 0) + (l2 != nullptr ? l2->val : 0) + march) / 10;
+        cur->val = digit;
+        cur->next = new ListNode();        
         l1 = l1->next == nullptr ? nullptr : l1->next;
         l2 = l2->next == nullptr ? nullptr : l2->next;
+        march = 0;
     }
+
+    return result;
 }
--- 2_addTwoNumbers/tests/addTwoNumbers_test.cpp
+++ 2_addTwoNumbers/tests/addTwoNumbers_test.cpp
@@ -1,15 +1,52 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "addTwoNumbers.h"
 
 int main()
-{
-    std::vector<int> days = {1,4,6,7,8,20};
-    std::vector<int> costs = {2,7,15};
-    int result = 0;
+{   
+    std::string num1;
+    std::string num2;
+
+    ListNode* l1 = new ListNode();
+    ListNode* l2 = new ListNode();
+    ListNode* result = 0;
+    int i = 0;
     Solution sol;
 
-    result = sol.mincostTickets(days, costs);
+    std::cout << "请输入第一个整数：";
+    std::cin >> num1;
+
+    std::cout << "请输入第二个整数：";
+    std::cin >> num2;
+
+    std::vector<ListNode*> line(num1.length(), nullptr);
+    for (char s : num1){
+        line[i] = new ListNode(int(s));
+        i++;
+    }
+
+    while(i > 0){
+        line[i]->next = line[i];
+        i--;
+    }
+    l1 = line.back();
+    for(ListNode* element : line) free(element);
+
+        for (char s : num2){
+        line[i] = new ListNode(int(s));
+        i++;
+    }
+
+    while(i > 0){
+        line[i]->next = line[i];
+        i--;
+    }
+    
+    l2 = line.back();
+    for(ListNode* element : line) free(element);
+
+    result = sol.addTwoNumbers(l1, l2);
 
     std::cout << result << "\n";
     return 0;
