8ee1c5bc89eb91ca40cf06b790760e9e61da78c1
zcc <cufezhoucc@163.com>
2023-09-01 16:15:55 +0800

    494 pause

---
+++ Dynamic Programming/494_findTargetSumWays/build/CMakeFiles/findTargetSumWays_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../findTargetSumWays_test"
+  "../findTargetSumWays_test.pdb"
+  "CMakeFiles/findTargetSumWays_test.dir/src/findTargetSumWays.cpp.o"
+  "CMakeFiles/findTargetSumWays_test.dir/src/findTargetSumWays.cpp.o.d"
+  "CMakeFiles/findTargetSumWays_test.dir/tests/findTargetSumWays_test.cpp.o"
+  "CMakeFiles/findTargetSumWays_test.dir/tests/findTargetSumWays_test.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/findTargetSumWays_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
