0fadda03a243f79280f6fa190dd3a6881ceda40c
zcc <cufezhoucc@163.com>
2023-09-05 22:50:00 +0800

    377 pause

--- Dynamic Programming/377_combinationSum4/src/combinationSum4.cpp
+++ Dynamic Programming/377_combinationSum4/src/combinationSum4.cpp
@@ -1,13 +1,27 @@
 #include "combinationSum4.h"
 
-int Solution::combinationSum4(std::vector<int>& nums, int target){
-    std::vector<int> dp(target+1, 0);
+// int Solution::combinationSum4(std::vector<int>& nums, int target){
+//     std::vector<int> dp(target+1, 0);
 
+//     dp[0] = 1;
+
+//     for(int num : nums){
+//         for(int j = num; j <= target; j++){
+//             dp[j] = dp[j - num] + dp[j];
+//         }
+//     }
+    
+//     return dp[target];
+// }
+
+int Solution::combinationSum4(std::vector<int>& nums, int target){
+    std::vector<int> dp(nums.size() + 1, 0);
+    std::sort(nums.begin(), nums.end());
     dp[0] = 1;
 
-    for(int num : nums){
-        for(int j = num; j <= target; j++){
-            dp[j] = dp[j - num] + dp[j];
+    for(int j = 0; j <= target; j++){
+        for(std::vector<int>::iterator it = nums.begin(); it != nums.end() && *it <= j; it++){
+            dp[*it] = dp[*it - j] + dp[*it];
         }
     }
     
