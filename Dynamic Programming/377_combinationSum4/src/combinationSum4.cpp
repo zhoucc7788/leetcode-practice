515c0df4f068726fad7dc09d2c2f4b6c0bf87640
zcc <cufezhoucc@163.com>
2023-09-06 22:32:04 +0800

    377 completed

--- Dynamic Programming/377_combinationSum4/src/combinationSum4.cpp
+++ Dynamic Programming/377_combinationSum4/src/combinationSum4.cpp
@@ -1,27 +1,13 @@
 #include "combinationSum4.h"
 
-// int Solution::combinationSum4(std::vector<int>& nums, int target){
-//     std::vector<int> dp(target+1, 0);
-
-//     dp[0] = 1;
-
-//     for(int num : nums){
-//         for(int j = num; j <= target; j++){
-//             dp[j] = dp[j - num] + dp[j];
-//         }
-//     }
-    
-//     return dp[target];
-// }
-
 int Solution::combinationSum4(std::vector<int>& nums, int target){
-    std::vector<int> dp(nums.size() + 1, 0);
-    std::sort(nums.begin(), nums.end());
+    std::vector<unsigned long long> dp(target + 1, 0);//小坑，虽然最终答案不超过32位，但中间的产物不一定
     dp[0] = 1;
 
     for(int j = 0; j <= target; j++){
-        for(std::vector<int>::iterator it = nums.begin(); it != nums.end() && *it <= j; it++){
-            dp[*it] = dp[*it - j] + dp[*it];
+        for(int num :nums){
+            if(num <= j)
+                dp[j] = dp[j - num] + dp[j];
         }
     }
     
